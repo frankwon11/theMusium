3751c7a15c758c021eebb74d42af6f3288854f72
frankwon11 <frankwon11@gmail.com>
2024-04-23 02:12:32 +0900

    Initial Commit

---
+++ theMusium/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  theMusium
+//
+//  Created by sseungwonnn on 4/23/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
